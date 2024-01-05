962b402b5d2a612637cd6da220c2840495d87647
amari <junior_maza16@hotmail.com>
2024-01-05 20:31:59 -0300

    - Json Dinamico a partir do CSV - #bug

--- src/test/java/apiTest/TesteUser.java
+++ src/test/java/apiTest/TesteUser.java
@@ -7,6 +7,8 @@ package apiTest;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 import java.io.IOException;
@@ -152,5 +154,47 @@ public class TesteUser {
         String token = response.jsonPath().getString("message").substring(23);
         System.out.println("Conteúdo do Token: " + token);
     }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "C:\\Users\\amari\\IdeaProjects\\Pascoa137\\src\\test\\resources\\csv\\massaUser.csv", numLinesToSkip = 1, delimiter = ',')
+    public void tertarIncluirUserCSV(
+            String id,
+            String username,
+            String firstName,
+            String lastName,
+            String email,
+            String password,
+            String phone,
+            String userStatus)
+    { // inicio incluir CSV
+        // carregar os dados do json
+        StringBuilder jsonBody = new StringBuilder("{");
+        jsonBody.append("'id': " + id + ",");
+        jsonBody.append("'username': " + username + "," );
+        jsonBody.append("'firstName': " + firstName + ",");
+        jsonBody.append("'lastName': " + lastName + ",");
+        jsonBody.append("'email': " + email + ",");
+        jsonBody.append("'password': " + password + ",");
+        jsonBody.append("'phone': " + phone + ",");
+        jsonBody.append("'userStatus': " + userStatus + ",");
+        jsonBody.append("}");
+
+
+        // realizar o teste
+        given()                                        // Dado que
+                .contentType(ct)    // o tipo de conteúdo
+                .log().all()                           // mostre tudo
+                .body(jsonBody)                        // corpo da requisição
+        .when()                                        // Quando
+                .post(uriUser) // Endpoint / Onde
+        .then()                                        // Então
+                .log().all()                           // mostre tudo na volta
+                .statusCode(200)                    // comunicação: ida e volta ok
+                .body("code", is(200))         // tag code é 200
+                .body("type", is("unknown"))   // tag type é "unknown
+                .body("message", is(id))         // messager é o userId
+        ;
+
+    }   // fim do incluir CSV
 
 }   // fim da classe
