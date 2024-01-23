4cb6930eab8786781c8cb51cd5526a2bb3f3e9c4
amari <junior_maza16@hotmail.com>
2024-01-23 17:53:31 -0300

    - Script do Selenium IDE importado e configurado com sucesso

---
+++ src/test/java/webTest/ComprarPassagemSIDE.java
@@ -0,0 +1,76 @@
+package webTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.*;
+
+public class ComprarPassagemSIDE {
+    private WebDriver driver;
+    private Map<String, Object> vars;
+    JavascriptExecutor js;
+    @BeforeEach
+    public void setUp() {
+        //WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        // Aponta onde está o Chrome Driver
+        System.setProperty("webdriver.chrome.driver", "drivers/chrome/chromedriver.exe");
+        driver = new ChromeDriver(options); // Instancia / Liga o chrome Driver
+        js = (JavascriptExecutor) driver;
+        vars = new HashMap<String, Object>();
+    }
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+    @Test
+    public void comprarPassagem() {
+        driver.get("https://www.blazedemo.com/");
+        driver.manage().window().setSize(new Dimension(1376, 736));
+        driver.findElement(By.name("fromPort")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("fromPort"));
+            dropdown.findElement(By.xpath("//option[. = 'São Paolo']")).click();
+        }
+        driver.findElement(By.name("toPort")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("toPort"));
+            dropdown.findElement(By.xpath("//option[. = 'Berlin']")).click();
+        }
+        driver.findElement(By.cssSelector(".btn-primary")).click();
+        assertThat(driver.findElement(By.cssSelector("h3")).getText(), is("Flights from São Paolo to Berlin:"));
+        driver.findElement(By.cssSelector("tr:nth-child(1) .btn")).click();
+        driver.findElement(By.id("inputName")).click();
+        driver.findElement(By.id("inputName")).sendKeys("Junior");
+        driver.findElement(By.id("address")).click();
+        driver.findElement(By.id("address")).sendKeys("Rua Caçador");
+        driver.findElement(By.id("city")).click();
+        driver.findElement(By.id("city")).sendKeys(" São Paulo");
+        driver.findElement(By.id("cardType")).click();
+        {
+            WebElement dropdown = driver.findElement(By.id("cardType"));
+            dropdown.findElement(By.xpath("//option[. = 'American Express']")).click();
+        }
+        driver.findElement(By.cssSelector(".checkbox")).click();
+        driver.findElement(By.cssSelector(".btn-primary")).click();
+        driver.findElement(By.cssSelector(".hero-unit")).click();
+        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Thank you for your purchase today!"));
+        assertThat(driver.findElement(By.cssSelector("tr:nth-child(3) > td:nth-child(2)")).getText(), is("555 USD"));
+    }
+}
+
+
